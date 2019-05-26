bb050664ce61f0aa612ca1c42728c888e0f66033
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-05-26 20:47:02 +0300

    Removal of conn.php and plain query
    The plain login validation query has been replaced with the usrLogin() function of the controller.

--- validate.php
+++ validate.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-include_once('_inc/conn.php');
-
 if($_SESSION['token'] != $_POST['token']){
     session_unset();
     session_destroy();
@@ -11,52 +9,19 @@ if($_SESSION['token'] != $_POST['token']){
 }
 
 if ($_SESSION['token']==$_POST['token']) {
-  if (time() >= $_SESSION['token_expire'] && time() >= $_SESSION['iddle_state']) {
+  if (time() >= $_SESSION['token_expire'] || time() >= $_SESSION['iddle_state']) {
     session_unset();
     session_destroy();
     header('Location: login.php');
-    exit();  
+    exit();
   } else {
-      
+
         $_SESSION['iddle_state'] = time() + 600;
-      
-        $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-        if ( mysqli_connect_errno() ) {
-        	
-        	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-        }
-        
-        
-        if ( !isset($_POST['username'], $_POST['password']) ) {
-            
-        	die ('Sorry! You must enter both with username and password. Thanks!!!');
-        }
-        
-        //To be checked for security
-        if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-        	
-        	$stmt->bind_param('s', $_POST['username']);
-        	$stmt->execute();
-        	$stmt->store_result();
-        }
-    
-        if ($stmt->num_rows > 0) {
-        	$stmt->bind_result($id, $password);
-        	$stmt->fetch();
-        	
-        	if (password_verify($_POST['password'], $password)) {
-        		session_regenerate_id();
-        		$_SESSION['useron'] = TRUE;
-        		$_SESSION['name'] = $_POST['username'];
-        		$_SESSION['id'] = $id;
-        		header('Location: index.php');
-        	} else {
-        		echo 'Incorrect password!';
-        	}
-        } else {
-        	echo 'Incorrect username!';
-        }
-        $stmt->close();
+
+        require_once("_inc/controller.php");
+
+        $db_handle = new SecureDB;
+        $db_handle->usrLogin($_POST['username'], $_POST['password']);
     }
 }
 ?>
