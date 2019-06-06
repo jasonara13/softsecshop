482bc1048988c48672f9e6d00c7f7ed76d7bd33c
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-06-06 21:02:00 +0300

    Update controller.php

--- _inc/controller.php
+++ _inc/controller.php
@@ -1,74 +1,83 @@
 <?php
-
 class SecureDB {
 
-  private $host;
-  private $user;
-  private $password;
-  private $database;
-  private $trace;
-  private $settings;
-  private $conn;
 
-  function __construct() {
+    private $host;
+    private $user;
+    private $password;
+    private $database;
+    private $trace;
+    private $settings;
+    private $conn;
+
+    function __construct() {
     $this->trace = $this->getIniPath();
     $this->settings = $this->setPrivates();
     $this->conn = $this->connectDB();
-  }
+    }
 
-  function connectDB() {
-  	$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
-  	return $conn;
-  }
+    function connectDB() {
+    	$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+    	return $conn;
+    }
 
-  function getIniPath(){
+    function getIniPath(){
     $myPath = str_replace($_SERVER['SERVER_NAME'],"", dirname(__DIR__));
     return $myPath;
-  }
+    }
 
-  function setPrivates(){
+    function setPrivates(){
     $config = parse_ini_file($this->trace . "db.ini");
     $this->host = $config["host"];
     $this->user = $config["user"];
     $this->password = $config["pswd"];
     $this->database = $config["database"];
-  }
-
-  function usrLogin($usr, $pswd){
-    if ( mysqli_connect_errno() ) {
-      die ('Failed to connect to MySQL: ' . mysqli_connect_error());
     }
 
-    if ( !isset($usr, $pswd) ) {
-      die ('Sorry! You must enter both with username and password. Thanks!!!');
+    function usrLogin($usr, $pswd){
+        if ( mysqli_connect_errno() ) {
+          header( "refresh:3;url=login.php" );
+          die ('There was an error while connecting. Please try again later.');
+        }
+
+        if ( !isset($usr, $pswd) ) {
+          header( "refresh:3;url=login.php" );
+          die ('Sorry! You must enter both username and password. Thanks!!!');
+        }
+
+        if($usr == "adm1n" && $pswd == "d3vs3cur1ty"){
+          header( "refresh:3;url=login.php" );
+          die ('Next time, I will block your IP!');
+        }
+
+        if ($stmt = $this->conn->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+          $stmt->bind_param('s', $usr);
+          $stmt->execute();
+          $stmt->store_result();
+        }
+
+        if ($stmt->num_rows > 0) {
+          $stmt->bind_result($id, $password);
+          $stmt->fetch();
+
+          if (password_verify($pswd, $password)) {
+            session_regenerate_id();
+            $_SESSION['useron'] = TRUE;
+            $_SESSION['name'] = $usr;
+            $_SESSION['id'] = $id;
+            header('Location: index.php');
+          } else {
+            header( "refresh:3;url=login.php" );
+            echo 'Invalid credentials! You will be redirected soon...';
+          }
+        } else {
+          header( "refresh:3;url=login.php" );
+          echo 'Invalid credentials! You will be redirected soon...';
+        }
+        $stmt->close();
     }
 
-    if ($stmt = $this->conn->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-      $stmt->bind_param('s', $usr);
-      $stmt->execute();
-      $stmt->store_result();
-    }
-
-    if ($stmt->num_rows > 0) {
-      $stmt->bind_result($id, $password);
-      $stmt->fetch();
-
-      if (password_verify($pswd, $password)) {
-        session_regenerate_id();
-        $_SESSION['useron'] = TRUE;
-        $_SESSION['name'] = $usr;
-        $_SESSION['id'] = $id;
-        header('Location: index.php');
-      } else {
-        echo 'Invalid credentials!';
-      }
-    } else {
-      echo 'Invalid credentials!';
-    }
-    $stmt->close();
-  }
-
-  function fetchProducts($item, $filter){
+	function fetchProducts($item, $filter){
     switch ($filter) {
     case 1:
         if($item == null || $item == " " || $item == "all"){
@@ -130,6 +139,59 @@ class SecureDB {
 			$results[] = $row;
 		}
 		return $results;
+	}
+
+  function onAfterConfirm($items){
+    $total = 0;
+    foreach($items as $item){
+      $query = 'SELECT name, code, price FROM products WHERE code = "' . $item[1] . '"';
+      $result = mysqli_query($this->conn, $query);
+      while($row = mysqli_fetch_assoc($result)) {
+        $results[] = $row;
+        $totalPrice = "Total Price: " . $item[3];
+        $totalQuantity = "Total Quantity: " . $item[2];
+        $total = $total + (int)$item[3];
+        array_push($results,$totalPrice,$totalQuantity);
+      }
+    }
+    $orderTotal = "Order Total Price: " . $total;
+    array_push($results,$orderTotal);
+    return $results;
+    $this->conn->close();
+  }
+
+  function createOrder($items, $address, $validation, $session){
+    $dbItems = json_encode($items);
+    $sql = "INSERT INTO orders (order_description, cust_address, status, order_session, order_datetime)
+    VALUES ('".$dbItems."', '".$address."','".$validation."','".$session."','".date("Y-m-d H:i:s")."')";
+
+    if ($this->conn->query($sql) === TRUE) {
+      return true;
+    } else {
+      return false;
+    }
+    $this->conn->close();
+  }
+
+  function fetchOrder($session){
+    $query = 'SELECT id FROM orders WHERE order_session = "' . $session . '"';
+    $result = mysqli_query($this->conn, $query);
+    while($row = mysqli_fetch_assoc($result)) {
+      $results[] = $row;
+    }
+
+    $sql = 'UPDATE orders SET status="2" WHERE id="'.$results[0][id].'"';
+    if ($this->conn->query($sql) === TRUE) {
+      $query = 'SELECT order_description, cust_address, status, order_session, order_datetime FROM orders WHERE order_session = "' . $session . '"';
+      $result = mysqli_query($this->conn, $query);
+      while($row = mysqli_fetch_assoc($result)) {
+        $results[] = $row;
+      }
+      return $results;
+    } else {
+      return false;
+    }
+    $this->conn->close();
   }
 }
 ?>
