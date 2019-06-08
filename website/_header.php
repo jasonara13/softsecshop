81a171d09dbbc334c1ece004b14bef0a0762a89a
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-06-08 15:29:15 +0300

    GCaptcha script added

--- website/_header.php
+++ website/_header.php
@@ -5,9 +5,9 @@
   <?php
   header("Strict-Transport-Security: max-age=31536000 ; includeSubDomains");
   header("X-Frame-Options: deny");
-  header("X-XSS-Protection: 1; mode=block");
+  //header("X-XSS-Protection: 1; mode=block");
   header("X-Content-Type-Options: nosniff");
-  header("Content-Security-Policy: script-src 'self'");
+  //header("Content-Security-Policy: script-src 'self'");
   header("X-Permitted-Cross-Domain-Policies: none");
   header("Referrer-Policy: no-referrer");
   header("Expect-CT: max-age=86400, enforce");
@@ -29,4 +29,5 @@
 <?php else: ?>
   <?php die('Something went wrong! Please check your directory and try again.'); ?>
 <?php endif; ?>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
