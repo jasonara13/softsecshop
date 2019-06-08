e22513ebece890907f39f99c60d2848bba3788c0
Alkaios Anagnostopoulos <36702495+Alcaeusd@users.noreply.github.com>
2019-06-08 15:23:25 +0300

    Update validate.php

--- website/validate.php
+++ website/validate.php
@@ -18,14 +18,20 @@ if(Token::tokenValidity($_POST['token'])){
     echo "Your session has expired. You will have to login again.";
     exit();
   } else {
+        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){
         Token::generateToken();
         $_SESSION['iddle_state'] = time() + 600;
         require_once("_inc/controller.php");
-
         $db_handle = new SecureDB;
         $userName = htmlspecialchars(htmlspecialchars($_POST['username'], ENT_QUOTES));
         $userPswd = htmlspecialchars(htmlspecialchars($_POST['password'], ENT_QUOTES));
         $db_handle->usrLogin($userName, $userPswd);
+        } else {
+                session_unset();
+                session_destroy();
+                header("refresh:1;url=login.php");
+                echo "Wrong Captcha";
+        }
     }
 }
 ?>
