6a16d41640473ef0d6fd45baf64033f318d2bc18
jasonara13 <51264216+jasonara13@users.noreply.github.com>
2019-06-09 17:05:40 +0300

    Update login.php

--- website/login.php
+++ website/login.php
@@ -5,6 +5,14 @@ $_SESSION['token_expire'] = time() + 3600;
 $_SESSION['iddle_state'] = time() + 600;
 
 if(time() >= $_SESSION['token_expire'] && time() >= $_SESSION['iddle_state']){
+    session_unset($_SESSION['useron']);
+session_unset($_SESSION['token']);
+session_unset($_SESSION['token_expire']);
+session_unset($_SESSION['iddle_state']);
+session_unset($_SESSION['cart-token']);
+session_unset($_SESSION['cart-checkout']);
+session_unset($_SESSION['name']);
+session_unset($_SESSION['id']);
     session_unset();
     session_destroy();
     header( "refresh:3;url=login.php" );
