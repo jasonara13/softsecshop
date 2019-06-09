364c5c7cc34e3e09275400f874c4340337074025
jasonara13 <51264216+jasonara13@users.noreply.github.com>
2019-06-09 17:04:05 +0300

    Update logout.php

--- website/logout.php
+++ website/logout.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+session_unset($_SESSION['useron']);
+session_unset($_SESSION['token']);
+session_unset($_SESSION['token_expire']);
+session_unset($_SESSION['iddle_state']);
+session_unset($_SESSION['cart-token']);
+session_unset($_SESSION['cart-checkout']);
+session_unset($_SESSION['name']);
+session_unset($_SESSION['id']);
+session_unset();
 session_destroy();
 header('Location: login.php');
 ?>
