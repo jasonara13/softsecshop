5e76311fbbb18de5ae00662eddd348cabcbf6be3
jasonara13 <51264216+jasonara13@users.noreply.github.com>
2019-06-09 17:15:56 +0300

    Update addtocart.php

--- website/addtocart.php
+++ website/addtocart.php
@@ -2,7 +2,15 @@
 session_start();
 
 if($_SESSION['token'] != $_POST['addToken']){
-    session_unset();
+	session_unset($_SESSION['useron']);
+	session_unset($_SESSION['token']);
+	session_unset($_SESSION['token_expire']);
+	session_unset($_SESSION['iddle_state']);
+	session_unset($_SESSION['cart-token']);
+	session_unset($_SESSION['cart-checkout']);
+	session_unset($_SESSION['name']);
+	session_unset($_SESSION['id']);
+	session_unset();
     session_destroy();
     header('Location: login.php');
 	exit();
@@ -10,14 +18,30 @@ if($_SESSION['token'] != $_POST['addToken']){
 else{
 
     if (!isset($_SESSION['useron'])) {
-        session_unset();
+        session_unset($_SESSION['useron']);
+	session_unset($_SESSION['token']);
+	session_unset($_SESSION['token_expire']);
+	session_unset($_SESSION['iddle_state']);
+	session_unset($_SESSION['cart-token']);
+	session_unset($_SESSION['cart-checkout']);
+	session_unset($_SESSION['name']);
+	session_unset($_SESSION['id']);
+	session_unset();
         session_destroy();
     	header('Location: login.php');
     	exit();
     }
 
     if(time() >= $_SESSION['token_expire'] && time() >= $_SESSION['iddle_state']){
-        session_unset();
+        session_unset($_SESSION['useron']);
+	session_unset($_SESSION['token']);
+	session_unset($_SESSION['token_expire']);
+	session_unset($_SESSION['iddle_state']);
+	session_unset($_SESSION['cart-token']);
+	session_unset($_SESSION['cart-checkout']);
+	session_unset($_SESSION['name']);
+	session_unset($_SESSION['id']);
+	session_unset();
         session_destroy();
         header('Location: login.php');
     	exit();
